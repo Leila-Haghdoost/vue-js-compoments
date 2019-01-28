0c24ac4a6bad0b9c8da1801886681436c4703b6e
Leila haghdoost <leilahaghdoost@192-168-1-6.tpgi.com.au>
2019-01-28 17:49:13 +1100

    looping through components

--- js/app.js
+++ js/app.js
@@ -15,5 +15,21 @@ Vue.component('card', {
 })
 
 new Vue({
-    el: '#app'
+    el: '#app',
+    data: {
+        articles: [{
+            title:'Build fullstack application with laravel and vuejs',
+            content:'ontrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000'
+
+        },
+        {
+            title:'Where does it come from?',
+            content: 'Sed ut perspiciatis unde isdt gdjei error sit voluptden accumnuyim dolorum larumenbd rem torem rem aperion eaque quae ab illo.'
+        },
+        {
+            title:'1914 translation by H. Rackham',
+            content: 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give.'
+        }
+    ]
+    }
 })
